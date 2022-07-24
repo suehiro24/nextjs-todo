d44e2166689683c2fb9e04402f7406f71027170b
Suehiro24 <hayatasato.0@gmail.com>
2022-07-24 17:12:54 +0900

    feat: Update index page with layouted tasklist

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import { Button } from '@mui/material'
 import type { NextPage } from 'next'
+import Layout from '../components/layout'
+import Tasklist from '../components/tasklist'
 
 const Home: NextPage = () => {
-  return <Button variant="contained">Hello World</Button>
+  return (
+    <Layout home={true} title={'Todo List'}>
+      <Tasklist></Tasklist>
+    </Layout>
+  )
 }
 
 export default Home
